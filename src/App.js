854c58bd8a1c450ad7169c432c7b4b64a6670556
mrchrischan <chanchris1117@gmail.com>
2020-01-29 02:10:39 -0500

    initializing project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from './Homepage.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
